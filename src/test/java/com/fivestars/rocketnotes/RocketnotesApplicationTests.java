6df90f688e8b578a886b38d27e5bdfae8edab6a7
ArianRM <arianramirezmendoza@gmail.com>
2024-09-11 01:51:18 -0500

    feat: admin management develop

---
+++ src/test/java/com/fivestars/rocketnotes/RocketnotesApplicationTests.java
@@ -0,0 +1,15 @@
+package com.fivestars.rocketnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
+@SpringBootTest
+@EnableJpaAuditing
+class RocketnotesApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+
+}
