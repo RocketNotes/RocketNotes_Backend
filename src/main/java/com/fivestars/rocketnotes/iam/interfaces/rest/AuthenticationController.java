13dc25a28cec1dfc8fe575a8c2fbf190aae5e01a
Lalo12342468 <u202018894@upc.edu.pe>
2024-09-19 19:21:19 -0500

    feat: cross origin auth

--- src/main/java/com/fivestars/rocketnotes/iam/interfaces/rest/AuthenticationController.java
+++ src/main/java/com/fivestars/rocketnotes/iam/interfaces/rest/AuthenticationController.java
@@ -12,11 +12,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value = "/api/v1/authentication", produces = MediaType.APPLICATION_JSON_VALUE)
 @Tag(name = "Authentication", description = "Authentication Endpoints")
